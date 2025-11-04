16b37cc72c40102c3b21623acd7c62e365173788
maelg <maelgarnier5@gmail.com>
2025-11-04 15:28:03 +0100

    correction style mobile

--- src/app/components/mainPage.tsx
+++ src/app/components/mainPage.tsx
@@ -29,10 +29,9 @@ export enum PageAlignmentEnum {
 }
 
 
+export default function MainPage({children, title, pageAlignment = PageAlignmentEnum.start}: pageProps) {
 
-export default function MainPage({children, title, pageAlignment=PageAlignmentEnum.start}: pageProps) {
-
-    const { scrollY } = useScroll();
+    const {scrollY} = useScroll();
     const [scrolled, setScrolled] = useState(false);
     const [element, setElement] = useState<Element | null>(null);
     const [section, setSection] = useState<Section | null>(null);
@@ -78,18 +77,20 @@ export default function MainPage({children, title, pageAlignment=PageAlignmentEn
 
     return (
         <main className={`flex items-start justify-start flex-col gap-6 ${title ? "pb-6 pl-6 pr-6" : "p-6"} mb-20`}>
-            {title && <div className={`flex flex-col ${scrolled ? "gap-4 pt-4" : "gap-4 pt-4 md:gap-6 md:pt-6"} sticky w-full top-0 left-0 z-10 bg-backgroundOpacity backdrop-blur`}>
-                <div className="flex gap-4 items-center justify-between w-full" >
+            {title && <div
+                className={`flex flex-col ${scrolled ? "gap-4 pt-4" : "gap-4 pt-4 md:gap-6 md:pt-6"} sticky w-full top-0 left-0 z-10 bg-backgroundOpacity backdrop-blur`}>
+                <div className="flex gap-4 items-center justify-between w-full">
                     <div className="flex gap-2 items-start flex-col justify-center">
                         <div className="flex gap-4 items-center">
                             <button
                                 className={`flex ${scrolled ? "gap-0" : "gap-0 md:gap-2"} items-center justify-center border-1 border-on-background-hover pl-4 pr-4 pt-2 pb-2 bg-onBackground rounded-lg md:hover:bg-onBackgroundHover active:bg-onBackgroundHover cursor-pointer`}
                                 onClick={() => router.back()}
                             >
-                                <img src="/ico/arrow-back.svg" alt="Back" className="w-4 invert" />
-                                <span className={`${scrolled ? "text-[0px]" : "text-[0px] md:text-[15px]"}`}>Retour</span>
+                                <img src="/ico/arrow-back.svg" alt="Back" className="w-4 invert"/>
+                                <span
+                                    className={`${scrolled ? "text-[0px]" : "text-[0px] md:text-[15px]"}`}>Retour</span>
                             </button>
-                            <h1 className={`line-clamp-2`} style={{fontSize: scrolled ? "20px" : "35px"}}>{title}</h1>
+                            <h1 className={`truncate w-full ${scrolled ? "text-[20px]" : "text-[20px] md:text-[35px]"}`}>{title}</h1>
                         </div>
 
                         <div
@@ -97,32 +98,35 @@ export default function MainPage({children, title, pageAlignment=PageAlignmentEn
                             {
                                 website &&
                                 <>
-                                    <a className={"cursor-pointer hover:underline"} href={"/secure/" + websiteId}>{StringUtil.truncateString(website.hero_title, 15)}</a>
+                                    <a className={"cursor-pointer hover:underline"}
+                                       href={"/secure/" + websiteId}>{StringUtil.truncateString(website.hero_title, 15)}</a>
                                 </>
                             }
                             {
                                 page &&
                                 <>
                                     <p>{" > "}</p>
-                                    <a className={"cursor-pointer hover:underline"} href={`/secure/${websiteId}/${pageId}`}>{StringUtil.truncateString(page.title, 15)}</a>
+                                    <a className={"cursor-pointer hover:underline"}
+                                       href={`/secure/${websiteId}/${pageId}`}>{StringUtil.truncateString(page.title, 15)}</a>
                                 </>
                             }
                             {
                                 section &&
                                 <>
                                     <p>{" > "}</p>
-                                    <a className={"cursor-pointer hover:underline"} href={`/secure/${websiteId}/${pageId}/${sectionId}`}>{StringUtil.truncateString(section.section_type, 15)}</a>
+                                    <a className={"cursor-pointer hover:underline"}
+                                       href={`/secure/${websiteId}/${pageId}/${sectionId}`}>{StringUtil.truncateString(section.section_type, 15)}</a>
                                 </>
                             }
                             {
                                 element &&
                                 <>
                                     <p>{" > "}</p>
-                                    <a className={"cursor-pointer hover:underline"} href={`/secure/${websiteId}/${pageId}/${sectionId}/${elementId}`}>{StringUtil.truncateString(element.content, 15)}</a>
+                                    <a className={"cursor-pointer hover:underline"}
+                                       href={`/secure/${websiteId}/${pageId}/${sectionId}/${elementId}`}>{StringUtil.truncateString(element.content, 15)}</a>
                                 </>
                             }
                         </div>
-
 
 
                     </div>
@@ -133,7 +137,7 @@ export default function MainPage({children, title, pageAlignment=PageAlignmentEn
                             className={`flex ${scrolled ? "gap-0" : "gap-0 md:gap-2"} items-center justify-center border-1 border-on-background-hover pl-4 pr-4 pt-2 pb-2 bg-onBackground rounded-lg md:hover:bg-onBackgroundHover active:bg-onBackgroundHover cursor-pointer`}
                             onClick={() => router.push("/secure")}
                         >
-                            <img src="/ico/home.svg" alt="power" className="w-4 invert" />
+                            <img src="/ico/home.svg" alt="power" className="w-4 invert"/>
                             <span className={`${scrolled ? "text-[0px]" : "text-[0px] md:text-[15px]"}`}>Accueil</span>
                         </button>
 
@@ -141,15 +145,17 @@ export default function MainPage({children, title, pageAlignment=PageAlignmentEn
                             className={`flex ${scrolled ? "gap-0" : "gap-0 md:gap-2"} items-center justify-center border-1 border-on-background-hover pl-4 pr-4 pt-2 pb-2 bg-onBackground rounded-lg md:hover:bg-onBackgroundHover active:bg-onBackgroundHover cursor-pointer`}
                             onClick={logout}
                         >
-                            <img src="/ico/power.svg" alt="power" className="w-4 invert" />
-                            <span className={`${scrolled ? "text-[0px]" : "text-[0px] md:text-[15px]"}`}>Déconnexion</span>
+                            <img src="/ico/power.svg" alt="power" className="w-4 invert"/>
+                            <span
+                                className={`${scrolled ? "text-[0px]" : "text-[0px] md:text-[15px]"}`}>Déconnexion</span>
                         </button>
                     </div>
 
                 </div>
                 <hr className={"border-0 h-[1px] w-full bg-onBackgroundHover"}/>
             </div>}
-            <div className={`p-3 w-full flex gap-5 ${pageAlignment === PageAlignmentEnum.center ? 'justify-center items-center flex-col' : pageAlignment === PageAlignmentEnum.start ? 'justify-start  items-stretch flex-col' : pageAlignment === PageAlignmentEnum.tileCenter ? 'justify-center  items-stretch flex-wrap flex-col md:flex-row' : 'justify-start  items-stretch flex-wrap md:flex-row'}`}>
+            <div
+                className={`md:p-3 p-1 w-full flex gap-5 ${pageAlignment === PageAlignmentEnum.center ? 'justify-center items-center flex-col' : pageAlignment === PageAlignmentEnum.start ? 'justify-start  items-stretch flex-col' : pageAlignment === PageAlignmentEnum.tileCenter ? 'justify-center  items-stretch flex-wrap flex-col md:flex-row' : 'justify-start  items-stretch flex-wrap md:flex-row'}`}>
                 {children}
             </div>
         </main>
